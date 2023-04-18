359b2df15aa85d23ba22c801c44029f57f7fd996
El Jefe <Tagne13@gmail.com>
2023-04-18 15:18:35 -0400

    set-up express and routes in server file

---
+++ server.js
@@ -0,0 +1,22 @@
+// Include dependencies & define PORT
+const express = require('express');
+const app = express ();
+const PORT = process.env.PORT || 3001;
+
+// Routers
+const apiRouter = require('./routes/apiRoutes');
+const htmlRouter = require('./routes/htmlRoutes');
+
+// To handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middleware
+app.use(express.static('public'));
+app.use(apiRouter);
+app.use(htmlRouter);
+
+// PORT check
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
